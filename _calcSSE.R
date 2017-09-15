b74a2e7a227e15a38446dcecc29200311f413c95
rb643 <rb643@cam.ac.uk>
2017-09-15 16:02:06 -0700

    update

--- _calcSSE.R
+++ _calcSSE.R
@@ -1,5 +1,5 @@
 # define function that returns the SSE for different local regression smoothing kernels
-calcSD <- function(z,x,y){
+calcSSE <- function(z,x,y){
   loessMod <- try(loess.sd(x ~ y, span=z), silent=T)
   res <- try(loessMod$model$residuals, silent=T)
   if(class(res)!="try-error"){
