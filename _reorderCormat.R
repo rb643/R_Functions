78fca4c26dd3c47fb7c4a65731475bff2a1012ad
rb643 <rb643@cam.ac.uk>
2017-09-15 12:54:51 -0700

    update

--- _reorderCormat.R
+++ _reorderCormat.R
@@ -1,4 +1,5 @@
 reorder_cormat <- function(cormat){
+  #function to return the reordering indices of a matrix based on hclust
 # Use correlation between variables as distance
 dd <- as.dist((1-cormat)/2)
 hc <- hclust(dd)
